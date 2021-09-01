327661e9c7e30f3aae1118349be3346504d50e18
Joonsung <gotohell.covid19@gmail.com>
2021-09-02 00:51:30 +0900

    Hehe
    fuck

---
+++ index.js
@@ -0,0 +1,19 @@
+function clickWin() {
+    alert("The Windows Logo was clicked!");
+}
+
+function shStartmenu() {
+    const tarDiv = document.getElementById("startmenu");
+    const trigger = document.getElementById("winlogo");
+
+    if (tarDiv.style.display !== "none") {
+        tarDiv.style.display = "none";
+    }
+    else {
+        tarDiv.style.display = "block";
+    }
+}
+
+function ie() {
+    alert("I'm sorry, but IE is dead.");
+}
